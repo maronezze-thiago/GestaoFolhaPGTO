4dabae6c25773d773672ace28bac48b05f998e9e
thiago <maronezzethiago@gmail.com>
2026-02-12 16:05:11 -0300

    criacao da classe abstrata funcionario na main

---
+++ gestaofinanceira3/src/gestaofinanceira3/funcionario.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestaofinanceira3;
+
+/**
+ *
+ * @author THIAGODEOLIVEIRAMARO
+ */
+public abstract class funcionario {
+    
+     private int id;
+    private String nome;
+    private String cpf;
+    private double salariobase;
+
+    public funcionario(int id, String nome, String cpf, double salariobase) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salariobase = salariobase;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalariobase() {
+        return salariobase;
+    }
+
+    public void setSalariobase(double salariobase) {
+        this.salariobase = salariobase;
+    }
+    
+    
+    
+    
+    
+    
+    
+    abstract void calcularsalario();
+    
+    
+    
+    
+}
